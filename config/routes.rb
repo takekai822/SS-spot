f68cc0c072a3868243e45fd8592d7295a478701a
takekai822 <kaikisoccer78@gmail.com>
2022-06-02 09:08:58 +0000

    ルートの記述の変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
+
   #管理者側
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
-    sessins: "admin/sessions"
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
