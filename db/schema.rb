3b655c54c343a7d4f0b5d35c75d58604ca31e0af
takekai822 <kaikisoccer78@gmail.com>
2022-06-03 01:47:08 +0000

    マイグレーションファイルのミス修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_004101) do
+ActiveRecord::Schema.define(version: 2022_06_03_014325) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,10 +46,6 @@ ActiveRecord::Schema.define(version: 2022_06_03_004101) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "name_kana"
-    t.string "user_name"
-    t.boolean "is_deleted", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
@@ -64,6 +60,10 @@ ActiveRecord::Schema.define(version: 2022_06_03_004101) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "name_kana"
+    t.string "user_name"
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
