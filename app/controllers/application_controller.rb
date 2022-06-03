30e42ddf5b0148961833b3becae7e83d9e1ecc2b
takekai822 <kaikisoccer78@gmail.com>
2022-06-03 09:59:28 +0000

    userのviewページ作成、コントローラーのミスの修正、userモデルに追加記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :user_name, :profile_image])
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,17 +1,21 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update, :edit]
-  
+
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.all
   end
 
   def edit
   end
-  
+
   def update
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   def favorites
@@ -22,15 +26,18 @@ class Public::UsersController < ApplicationController
 
   def quit
   end
-  
+
+  def withdraw
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :name_kana, :user_name, :profile_image, :email )
   end
-  
+
   def ensure_correct_user
-    @user = User.find(pamras[:id])
+    @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(@user)
     end
