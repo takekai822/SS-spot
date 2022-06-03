e4aff4ccc04e2ff9e6104d45d7cac649c2b40088
takekai822 <kaikisoccer78@gmail.com>
2022-06-03 02:25:50 +0000

    public/searchesコンとローラーとviewページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'searches/search'
+  end
+  namespace :public do
     get 'users/show'
     get 'users/edit'
     get 'users/favorites'
