8a843c3921ea2d7b83facce9f2116b46c2adc500
takekai822 <kaikisoccer78@gmail.com>
2022-06-04 06:38:51 +0000

    postの基本的な機能追加、タグのgem追加、viewページ作成

--- Gemfile
+++ Gemfile
@@ -55,5 +55,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari','~> 1.2.1'
-
+gem 'acts-as-taggable-on', '~> 9.0'
 gem 'devise'
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,59 @@
 class Public::PostsController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to post_path(@post)
+    else
+      render "new"
+    end
   end
 
   def index
+    @posts = Post.all
+    #タグの絞り込みを行う
+    if params[:tag_name]
+      #タグがクリックされたらクリックされたタグの名前をtagge_with(タグの名前)メソッドで検索し絞り込みを行う
+      @posts = Post.tagged_with("#{params[:tag_name]}")
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "編集内容を更新しました"
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :address, :latitude, :longitude, :tag_list, post_images: [])
+  end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to request.referer
+    end
   end
 end
