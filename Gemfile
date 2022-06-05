d65aa6aa6b45bc2e07643cae4ffb3f061f47258b
takekai822 <kaikisoccer78@gmail.com>
2022-06-05 06:25:42 +0000

    google_map_apiの導入、viewページの変更

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari','~> 1.2.1'
 gem 'acts-as-taggable-on', '~> 9.0'
 gem 'devise'
+gem 'dotenv-rails'
+gem "geocoder"
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,28 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many_attached :post_images
-  
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   validates :title, presence: true
   validates :body, presence: true
   validates :post_images, presence: true
-  
+  validates :address, presence: true
+  validates :tag_list, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validate :validate_number_of_files
+
+  private
+  #投稿画像の枚数制限のためのメソッド
+  def validate_number_of_files
+    return if post_images.length <= 5
+    errors.add(:post_images, "に添付できる画像は5枚までです。")
+  end
+
   #タグ情報を保存する
   acts_as_taggable
 end
