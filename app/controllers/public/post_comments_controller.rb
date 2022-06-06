674bb2c1554de7e258c0c999a6d1445dce74f943
takekai822 <kaikisoccer78@gmail.com>
2022-06-06 07:29:30 +0000

    コメント機能の実装

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,2 +1,25 @@
 class Public::PostCommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    if @post_comment.save
+      render "public/post_comments/post_comment"
+    else
+      render "public/posts/show"
+    end
+  end
+
+  def destroy
+    @post_comment = PostComment.find(params[:id]).destroy
+    @post = Post.find(params[:post_id])
+    render "public/post_comments/post_comment"
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment, :rate)
+  end
+
 end
