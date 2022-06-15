e13ba114d0d9d0a8efba19e63b2fa8fe5a0e987f
takekai822 <kaikisoccer78@gmail.com>
2022-06-15 06:44:59 +0000

    レイアウト変更６回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # アソシエーション
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # バリデーション
   validates :name, presence: true, length: {minimum: 2, maximum: 30}
   validates :name_kana, presence: true, length: {minimum: 2,maximum: 30}
   validates :user_name, presence: true, length: {minimum: 2,maximum: 30}
 
+  # プロフィール画像を登録する際に使用
   has_one_attached :profile_image
 
-  #プロフィール画像の表示に関するメソッド
+  # プロフィール画像の表示に関するメソッド
   def get_profile_image
-    #プロフィール画像が添付されているかを確認し、添付されていた場合その画像を表示し、添付されていない場合はno_image.jpgを表示
+    # プロフィール画像が添付されているかを確認し、添付されていた場合その画像を表示し、添付されていない場合はno_image.jpgを表示
     (profile_image.attached?) ? profile_image : "no_image.jpg"
   end
 end
