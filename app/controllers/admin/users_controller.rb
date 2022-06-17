0ca425bdf4cf5831423d1f397012feff5c68e689
takekai822 <kaikisoccer78@gmail.com>
2022-06-17 12:33:10 +0000

    レイアウト変更１２回目

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,16 @@ class Admin::UsersController < ApplicationController
   # 管理者側 ユーザー一覧
   def index
     @users = User.all.page(params[:page]).per(10)
+    # ユーザー数
+    @user_count = User.all.count
   end
 
   # 管理者側 ユーザー詳細
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.all.page(params[:page]).per(10)
+    # ユーザーの投稿数
+    @post_count = @user.posts.all.count
   end
 
   # 管理者側 ユーザー編集
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,5 +2,7 @@ class Public::SearchesController < ApplicationController
   # 検索
   def search
     @posts = Post.looks(params[:word]).page(params[:page]).per(10)
+    # 投稿数
+    @post_count = Post.looks(params[:word]).count
   end
 end
